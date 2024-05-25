5c7870f8203b8e30dbd58c0ba1bcadbdc9408dc9
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-05-25 07:06:47 -1200

    Initial commit

---
+++ app/src/main/java/com/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+
+class MainViewModel(private val dataStore: DataStore<Preferences>) {
+
+
+
+}
---
+++ app/src/main/java/com/example/datastorelib/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.datastorelib
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.lifecycleScope
+import com.example.datastorelib.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+}
